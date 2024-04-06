c372af9e04ff128c561abf828785d29a9962097b
Yusuf Arslan <yusufarsln98@outlook.com>
2024-04-06 20:16:43 +0300

    feat: types for user and vending machines

--- types/index.ts
+++ types/index.ts
@@ -3,3 +3,37 @@ import { SVGProps } from 'react';
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
+
+export type Dispenser = {
+  id: number;
+  index: number;
+  stock: number;
+  productId?: number;
+  vendingMachineId?: number;
+};
+
+export type User = {
+  id: number;
+  username: string;
+  password?: string;
+  name?: string;
+  token?: string;
+  role: string;
+};
+
+export type UserCredentials = Pick<User, 'username' | 'password' | 'name'>;
+export type UserResponse = Pick<User, 'id' | 'username' | 'role' | 'token'>;
+export type Message = {
+  message: string;
+};
+
+export type AuthAction = (
+  newUser: UserCredentials,
+  setError: React.Dispatch<React.SetStateAction<Message>>,
+) => Promise<void>;
+
+export type VendingMachine = {
+  id: number;
+  name: string;
+  dispensers: Dispenser[];
+};
