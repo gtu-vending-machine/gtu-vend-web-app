e29c34cc50d79b3d342e7bf30ed4ce2c08c018c4
Yusuf Arslan <yusufarsln98@outlook.com>
2024-04-06 20:30:45 +0300

    feat: default paddings are removed

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -4,8 +4,6 @@ export default function AboutLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className='flex flex-col items-center justify-center gap-4 py-8 md:py-10'>
-      {children}
-    </section>
+    <section className='flex flex-col items-center gap-4'>{children}</section>
   );
 }
