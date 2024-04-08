201b30051ed2ec8cd3c13db51bd488d357bd62a7
Yusuf Arslan <yusufarsln98@outlook.com>
2024-04-08 22:50:54 +0300

    feat: table outline component to reusability.

---
+++ components/admin/table/table-outline.tsx
@@ -0,0 +1,159 @@
+'use client';
+
+import React, {
+  Dispatch,
+  JSXElementConstructor,
+  ReactElement,
+  SetStateAction,
+  useMemo,
+  useState,
+} from 'react';
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  Selection,
+  SortDescriptor,
+  TableBodyProps,
+} from '@nextui-org/react';
+import { Column, OptionType, Query } from '@/types';
+
+import useTopContent from './hooks/useTopContent';
+import useBottomContent from './hooks/useBottomContent';
+import useFilterCallbacks from './hooks/useFilterCallbacks';
+import usePageCallbacks from './hooks/usePageCallBacks';
+
+// export default function UserTable({
+export default function TableOutline<T extends { id: number; name: string }>({
+  data,
+  count,
+  columns,
+  query,
+  setQuery,
+  options,
+  searchOption,
+  selectionOption,
+  children,
+}: {
+  data: T[];
+  count: number;
+  columns: Column[];
+  query: Query;
+  setQuery: Dispatch<SetStateAction<Query>>;
+  options: OptionType[]; // option for filtering (on header)
+  searchOption: OptionType;
+  selectionOption?: OptionType;
+  children: ReactElement<
+    TableBodyProps<object>,
+    string | JSXElementConstructor<any>
+  >;
+}) {
+  const [page, setPage] = useState(1);
+  const [searchValue, setsearchValue] = useState('');
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const pages = Math.max(1, Math.ceil(count / rowsPerPage));
+  const [optionSelection, setOptionSelection] = useState<Selection>('all');
+  const [sortDescriptor, setSortDescriptor] = useState<SortDescriptor>({});
+
+  // const [selectedKeys, setSelectedKeys] = useState<Selection>(new Set([]));
+  const [visibleColumns, setVisibleColumns] = useState<Selection>(
+    new Set(columns.map((column) => column.uid)),
+  );
+
+  const headerColumns = useMemo(() => {
+    if (visibleColumns === 'all') return columns;
+
+    return columns.filter((column) =>
+      Array.from(visibleColumns).includes(column.uid),
+    );
+  }, [visibleColumns]);
+
+  const { onRowsPerPageChange, onPageChange, onPreviousPage, onNextPage } =
+    usePageCallbacks<T>({
+      page,
+      pages,
+      setPage,
+      setQuery,
+      rowsPerPage,
+      setRowsPerPage,
+    });
+
+  const {
+    onSearchValueChange,
+    onOptionSelectionChange,
+    onClear,
+    onSortChange,
+  } = useFilterCallbacks({
+    setPage,
+    setQuery,
+    rowsPerPage,
+    setsearchValue,
+    query,
+    setOptionSelection,
+    setSortDescriptor,
+    options,
+    searchOption,
+    selectionOption,
+  });
+
+  const topContent = useTopContent({
+    count,
+    columns,
+    options, // optional
+    searchValue,
+    optionSelection, // optional
+    visibleColumns,
+    searchOption,
+    selectionOption,
+    onSearchValueChange,
+    onOptionSelectionChange, // optional
+    onRowsPerPageChange,
+    onClear,
+    setVisibleColumns,
+  });
+
+  const bottomContent = useBottomContent({
+    // selectedKeys, // optional
+    count,
+    page,
+    pages,
+    onPageChange,
+    onPreviousPage,
+    onNextPage,
+  });
+
+  return (
+    <>
+      <Table
+        aria-label='Example table with custom cells, pagination and sorting'
+        isHeaderSticky
+        bottomContent={bottomContent}
+        bottomContentPlacement='outside'
+        classNames={{
+          wrapper: 'max-h-[556px]',
+        }}
+        // selectedKeys={selectedKeys}
+        // selectionMode='multiple'
+        sortDescriptor={sortDescriptor}
+        topContent={topContent}
+        topContentPlacement='outside'
+        // onSelectionChange={setSelectedKeys}
+        onSortChange={onSortChange}
+        className='w-full p-4'
+      >
+        <TableHeader columns={headerColumns}>
+          {(column) => (
+            <TableColumn
+              key={column.uid}
+              align={column.uid === 'actions' ? 'center' : 'start'}
+              allowsSorting={column.sortable}
+            >
+              {column.name}
+            </TableColumn>
+          )}
+        </TableHeader>
+        {children}
+      </Table>
+    </>
+  );
+}
--- components/admin/users/index.tsx
+++ components/admin/users/index.tsx
@@ -1,109 +1,41 @@
 'use client';
 
-import React, { useMemo, useState } from 'react';
-import {
-  Table,
-  TableHeader,
-  TableColumn,
-  TableBody,
-  TableRow,
-  TableCell,
-  Selection,
-  SortDescriptor,
-  Spinner,
-} from '@nextui-org/react';
-import { Column, Query, UserListItem } from '@/types';
+import React, { Dispatch, SetStateAction, useState } from 'react';
+import { TableBody, TableRow, TableCell, Spinner } from '@nextui-org/react';
+import { Column, OptionType, Query } from '@/types';
+import { UserListItem } from '@/types/user';
 
-import useRenderCell from './hooks/useRenderCell';
-import useTopContent from './hooks/useTopContent';
-import useBottomContent from './hooks/useBottomContent';
-import useCallBacks from './hooks/useCallBacks';
+import useRenderCell from '../table/hooks/useRenderCell';
 import UserDetailDrawer from './user-details';
+import TableOutline from '../table/table-outline';
 
-export default function Users({
-  users,
-  setUsers,
+const searchOption: OptionType = { uid: 'name', name: 'name' };
+// supports only two options for now
+const selectionOptions: OptionType[] = [
+  { uid: 'admin', name: 'admin' },
+  { uid: 'user', name: 'user' },
+];
+const selectionOption: OptionType = { uid: 'role', name: 'role' };
+
+export default function UserTable({
+  data,
+  setData,
   loading,
   count,
   columns,
   query,
   setQuery,
 }: {
-  users: UserListItem[];
-  setUsers: React.Dispatch<React.SetStateAction<UserListItem[]>>;
+  data: UserListItem[];
+  setData: Dispatch<SetStateAction<UserListItem[]>>;
   loading: boolean;
   count: number;
   columns: Column[];
-  query: Query<UserListItem>;
-  setQuery: React.Dispatch<React.SetStateAction<Query<UserListItem>>>;
+  query: Query;
+  setQuery: Dispatch<SetStateAction<Query>>;
 }) {
-  const [page, setPage] = useState(1);
-  const [filterValue, setFilterValue] = useState('');
-  const [rowsPerPage, setRowsPerPage] = useState(5);
-  const pages = Math.max(1, Math.ceil(count / rowsPerPage));
-  const [roleFilter, setRoleFilter] = useState<Selection>('all');
-  const [sortDescriptor, setSortDescriptor] = useState<SortDescriptor>({});
-
-  const [selectedKeys, setSelectedKeys] = useState<Selection>(new Set([]));
-  const [visibleColumns, setVisibleColumns] = useState<Selection>(
-    new Set(columns.map((column) => column.uid)),
-  );
-
   const [isOpen, setIsOpen] = useState(false);
   const [userId, setUserId] = useState<number>(0);
-
-  const headerColumns = useMemo(() => {
-    if (visibleColumns === 'all') return columns;
-
-    return columns.filter((column) =>
-      Array.from(visibleColumns).includes(column.uid),
-    );
-  }, [visibleColumns]);
-
-  const {
-    onSearchChange,
-    onRoleSelectionChange,
-    onRowsPerPageChange,
-    onClear,
-    onPageChange,
-    onPreviousPage,
-    onNextPage,
-    onSortChange,
-  } = useCallBacks({
-    page,
-    pages,
-    setPage,
-    setQuery,
-    rowsPerPage,
-    setRowsPerPage,
-    setFilterValue,
-    query,
-    setRoleFilter,
-    setSortDescriptor,
-  });
-
-  const topContent = useTopContent({
-    filterValue,
-    onSearchChange,
-    roleFilter,
-    onRoleSelectionChange,
-    visibleColumns,
-    columns,
-    users,
-    onRowsPerPageChange,
-    onClear,
-    setVisibleColumns,
-  });
-
-  const bottomContent = useBottomContent({
-    selectedKeys,
-    users,
-    page,
-    pages,
-    onPageChange,
-    onPreviousPage,
-    onNextPage,
-  });
 
   const renderCell = useRenderCell({
     setUserId,
@@ -112,37 +44,19 @@ export default function Users({
 
   return (
     <>
-      <Table
-        aria-label='Example table with custom cells, pagination and sorting'
-        isHeaderSticky
-        bottomContent={bottomContent}
-        bottomContentPlacement='outside'
-        classNames={{
-          wrapper: 'max-h-[556px]',
-        }}
-        // selectedKeys={selectedKeys}
-        // selectionMode='multiple'
-        sortDescriptor={sortDescriptor}
-        topContent={topContent}
-        topContentPlacement='outside'
-        onSelectionChange={setSelectedKeys}
-        onSortChange={onSortChange}
-        className='w-full p-4'
+      <TableOutline<UserListItem>
+        columns={columns}
+        data={data}
+        count={count}
+        query={query}
+        setQuery={setQuery}
+        options={selectionOptions}
+        searchOption={searchOption}
+        selectionOption={selectionOption}
       >
-        <TableHeader columns={headerColumns}>
-          {(column) => (
-            <TableColumn
-              key={column.uid}
-              align={column.uid === 'actions' ? 'center' : 'start'}
-              allowsSorting={column.sortable}
-            >
-              {column.name}
-            </TableColumn>
-          )}
-        </TableHeader>
         <TableBody
-          emptyContent={'No users found'}
-          items={users}
+          emptyContent={'No data found'}
+          items={data}
           loadingContent={<Spinner />}
           loadingState={loading ? 'loading' : 'idle'}
         >
@@ -154,12 +68,12 @@ export default function Users({
             </TableRow>
           )}
         </TableBody>
-      </Table>
+      </TableOutline>
       <UserDetailDrawer
         setIsOpen={setIsOpen}
         isOpen={isOpen}
-        user={users.find((user) => user.id === userId)}
-        setUsers={setUsers}
+        user={data.find((user) => user.id === userId)}
+        setUsers={setData}
       />
     </>
   );
