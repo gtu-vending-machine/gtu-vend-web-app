8a8d6e358d4b631c2a92eebca8b7aa71986d3c34
Yusuf Arslan <yusufarsln98@outlook.com>
2024-04-10 17:40:10 +0300

    feat: slot card inside the drawer

---
+++ components/admin/vending-machines/slot-card-body.tsx
@@ -0,0 +1,105 @@
+'use client';
+
+import {
+  Button,
+  Avatar,
+  CardHeader,
+  CardFooter,
+  Divider,
+  CardBody,
+  Badge,
+} from '@nextui-org/react';
+
+import { PlusIcon } from '@/components/icons';
+import { Slot } from '@/types/vending-machines';
+
+const FullSlot = ({
+  setSelectedSlot,
+  slot,
+  onOpen,
+}: {
+  setSelectedSlot: React.Dispatch<React.SetStateAction<Slot | undefined>>;
+  slot: Slot;
+  onOpen: () => void;
+}) => {
+  if (!slot.product) {
+    return null;
+  }
+  return (
+    <>
+      <CardHeader className='p-0 flex w-full items-center justify-center'>
+        <div
+          className='transition-transform transform hover:scale-110 cursor-pointer'
+          onClick={() => {
+            setSelectedSlot(slot);
+            onOpen();
+          }}
+        >
+          <Badge
+            color='default'
+            className='flex items-center justify-center w-6 h-6'
+            content={slot.stock > 9 ? '9+' : slot.stock}
+            title='Stock'
+          >
+            <Avatar
+              radius='sm'
+              src={slot.product?.image}
+              name={slot.product?.name}
+              isBordered
+            />
+          </Badge>
+        </div>
+      </CardHeader>
+      <CardBody className='p-0'>
+        <p className='text-ellipsis overflow-hidden whitespace-nowrap text-sm text-gray-500'>
+          {slot.product?.name}
+        </p>
+      </CardBody>
+      <CardFooter className='flex items-center justify-center p-0'>
+        <div className='flex w-full flex-col justify-center items-center'>
+          <Divider />
+          {slot.index}
+        </div>
+      </CardFooter>
+    </>
+  );
+};
+const EmptySlot = ({
+  setSelectedSlot,
+  slot,
+  onOpen,
+}: {
+  setSelectedSlot: React.Dispatch<React.SetStateAction<Slot | undefined>>;
+  slot: Slot;
+  onOpen: () => void;
+}): React.ReactNode | React.ReactNode[] => {
+  if (slot.product) {
+    return null;
+  }
+  return (
+    <>
+      <CardBody className='p-0 flex items-center justify-between'>
+        <Button
+          variant='light'
+          onClick={() => {
+            setSelectedSlot(slot);
+            onOpen();
+          }}
+        >
+          <PlusIcon />
+        </Button>
+        <p className='text-ellipsis overflow-hidden whitespace-nowrap text-sm text-gray-500'>
+          Empty
+        </p>
+      </CardBody>
+      <CardFooter className='flex items-center justify-center p-0'>
+        <div className='flex w-full flex-col justify-center items-center'>
+          <Divider />
+          {slot.index}
+        </div>
+      </CardFooter>
+    </>
+  );
+};
+
+export { FullSlot, EmptySlot };
