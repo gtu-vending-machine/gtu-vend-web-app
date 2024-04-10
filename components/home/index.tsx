a14ded9b0aa1d853de36a1b804fcd7683a732592
Yusuf Arslan <yusufarsln98@outlook.com>
2024-04-10 21:03:14 +0300

    feat: home page seperated to different components

---
+++ components/home/index.tsx
@@ -0,0 +1,120 @@
+'use client';
+
+import { SearchIcon } from '@/components/icons';
+import { AdminApiContext } from '@/context/admin-api-provider';
+import { Query } from '@/types';
+import { VendingMachineListItem } from '@/types/vending-machines';
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Input,
+  Pagination,
+  Spinner,
+} from '@nextui-org/react';
+import { useContext, useEffect, useState } from 'react';
+import { HasVendingMachine, NoVendingMachine } from './vending-machine-list';
+
+const PAGE_SIZE = 8;
+
+export const Content = () => {
+  const { getVendingMachinesWithQuery } = useContext(AdminApiContext);
+  const [vendingmachines, setVendingMachines] = useState<
+    VendingMachineListItem[]
+  >([]);
+  const [query, setQuery] = useState<Query>({
+    query: {
+      pagination: { page: 1, pageSize: PAGE_SIZE },
+    },
+  });
+  const [count, setCount] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const fetchVendingMachines = async (q: Query) => {
+    setLoading(true);
+    const data = await getVendingMachinesWithQuery(q);
+    if (data) {
+      setVendingMachines(data.vendingMachines);
+      setCount(data._count);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchVendingMachines(query);
+  }, [query]);
+
+  console.log(vendingmachines);
+
+  return (
+    <div className='flex flex-col items-center justify-center gap-4 py-8'>
+      <div className='max-w-xl w-full gap-4 flex flex-col'>
+        <Input
+          placeholder='Search a vending machine...'
+          variant='underlined'
+          startContent={<SearchIcon color='gray' />}
+          value={(query.query?.filter?.[0]?.value as string) || ''}
+          onChange={(e) =>
+            setQuery({
+              query: {
+                pagination: { page: 1, pageSize: PAGE_SIZE },
+                filter: [
+                  {
+                    field: 'name',
+                    option: 'contains',
+                    value: e.target.value,
+                  },
+                ],
+              },
+            })
+          }
+        />
+        <div className='flex flex-col justify-center items-center gap-5'>
+          <Card shadow='none' className='border border-gray-200 w-full'>
+            <CardHeader>Vending Machines</CardHeader>
+            <CardBody>
+              {loading ? (
+                <div className='flex justify-center items-center  mt-20'>
+                  <Spinner />
+                </div>
+              ) : (
+                <>
+                  <HasVendingMachine
+                    vendingmachines={vendingmachines}
+                    page={query.query.pagination?.page as number}
+                  />
+                  <NoVendingMachine vendingmachines={vendingmachines} />
+                </>
+              )}
+            </CardBody>
+            <CardFooter className='flex flex-col gap-4'>
+              <div className='flex w-full mt-4'>
+                <p className='text-xs text-gray-400 text-center'>
+                  Total {count} vending machines.
+                </p>
+              </div>
+              {count > 0 && (
+                <div>
+                  <Pagination
+                    onChange={(page) =>
+                      setQuery({
+                        query: {
+                          ...query.query,
+                          pagination: { page, pageSize: PAGE_SIZE },
+                        },
+                      })
+                    }
+                    showControls
+                    initialPage={1}
+                    total={Math.ceil(count / PAGE_SIZE)}
+                  />
+                </div>
+              )}
+            </CardFooter>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ components/home/vending-machine-list.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import { VendingMachineListItem } from '@/types/vending-machines';
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Divider,
+} from '@nextui-org/react';
+import Link from 'next/link';
+
+const PAGE_SIZE = 8;
+
+const HasVendingMachine = ({
+  vendingmachines,
+  page,
+}: {
+  vendingmachines: VendingMachineListItem[];
+  page: number;
+}) => {
+  if (vendingmachines.length === 0) return;
+
+  return (
+    <div className='w-full grid sm:grid-cols-4 gap-4 mt-4 grid-cols-2'>
+      {vendingmachines.map((vendingmachine, index) => {
+        return (
+          <Card shadow='sm' className='cursor-pointer' key={vendingmachine.id}>
+            <Link
+              href={`/vending-machines/${vendingmachine.id}`}
+              className='w-full h-full'
+            >
+              <CardHeader>
+                <p
+                  title={vendingmachine.name}
+                  className='overflow-ellipsis overflow-hidden whitespace-nowrap'
+                >
+                  {vendingmachine.name}
+                </p>
+              </CardHeader>
+              <CardBody>
+                <div className='flex flex-col gap-2'>
+                  <p>
+                    <span className='font-light'>
+                      total slots: {vendingmachine._slotCount}
+                    </span>
+                  </p>
+                </div>
+              </CardBody>
+              <CardFooter>
+                <div className='flex flex-col w-full items-center'>
+                  <Divider />
+                  {/* {index + 1} */}
+                  {index + 1 + (page - 1) * PAGE_SIZE}
+                </div>
+              </CardFooter>
+            </Link>
+          </Card>
+        );
+      })}
+    </div>
+  );
+};
+
+const NoVendingMachine = ({ vendingmachines }: { vendingmachines: any[] }) => {
+  if (vendingmachines.length > 0) return;
+  return (
+    <div className='flex justify-center items-center w-full mt-20'>
+      <h1>No vending machines found</h1>
+    </div>
+  );
+};
+
+export { HasVendingMachine, NoVendingMachine };
