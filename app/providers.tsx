ddf5bb0118a0ea4e55dd98782658114e7b89e659
Yusuf Arslan <yusufarsln98@outlook.com>
2024-05-26 01:45:19 +0300

    fix: wrap api provider to app

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/navigation';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { ThemeProviderProps } from 'next-themes/dist/types';
 import { AuthProvider } from '@/context/auth-provider';
+import { ApiProvider } from '@/context/api-provider';
 
 export interface ProvidersProps {
   children: React.ReactNode;
@@ -18,7 +19,9 @@ export function Providers({ children, themeProps }: ProvidersProps) {
   return (
     <NextUIProvider navigate={router.push}>
       <NextThemesProvider {...themeProps}>
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          <ApiProvider>{children}</ApiProvider>
+        </AuthProvider>
       </NextThemesProvider>
     </NextUIProvider>
   );
